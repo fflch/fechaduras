8ee9565be50d6b566232d285728f5360b90b7b3d
Alan Neves <alan.n.ribeiro257@hotmail.com>
2025-05-20 11:50:25 -0300

    instala tema usp

---
+++ app/Http/Requests/FechaduraRequest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class FechaduraRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        $rules = [
+            'local' => 'required',
+            'ip' => ['required', 'ipv4', 'unique:fechaduras,ip'], 
+            'usuario' => 'required',
+            'senha' => 'sometimes|required'
+        ];
+
+        if ($this->method() == 'PATCH' || $this->method() == 'PUT') {
+            $rules['ip'] = ['required', 'ipv4', 'unique:fechaduras,ip,' . $this->fechadura->id];
+        }
+    
+        return $rules;
+    }
+
+    protected function prepareForValidation()
+    {
+        $this->merge([
+            'ip' => str_replace('-','',$this->ip)
+        ]);
+    }
+
+    public function messages()
+    {
+        return [
+            'local.required' => 'O local da fechadura é obrigatório',
+            'ip.required' => 'O IP é obrigatório',
+            'ip.ipv4' => 'Digite um IP válido',
+            'ip.unique' => 'Este IP já está cadastrado',
+            'usuario.required' => 'O usuário é obrigatório',
+            'senha.required' => 'A senha é obrigatória'
+        ];
+    }
+}
--- app/Models/Fechadura.php
+++ app/Models/Fechadura.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Fechadura extends Model
 {
-    //
+    protected function local(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value) => " " . $value,
+            set: fn($value) => " " . $value
+        );
+    }
+
+    public function acessos() {
+        return $this->hasMany(Acesso::class);
+    }
 }
