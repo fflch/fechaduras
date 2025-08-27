13d44d3f4a27ee990670fb8454c9b7f55811494b
Ricardo Fontoura <ricardfo@usp.br>
2025-08-27 15:21:52 -0300

    ajustes nas variáveis da sessão.

--- app/Services/LockSessionService.php
+++ app/Services/LockSessionService.php
@@ -4,15 +4,20 @@ namespace App\Services;
 
 use Illuminate\Support\Facades\Http;
 use App\Exceptions\ConnectionFailureException;
+use Illuminate\Support\Str;
 
 class LockSessionService
 {
+    private const LOCK_SESSION = 'lock:session:';
+
     // Obtém ou cria uma sessão ativa com a fechadura
     public static function conexao($ip, $usuario, $senha){
+        $lock_session = self::getLockSession($ip);
         try {
-            if (session()->has('lock:session')) {
-                return self::validade($ip)
-                    ? session()->get('lock:session') : self::login($ip, $usuario, $senha);
+            if (session()->has($lock_session)) {
+                $session = session()->get($lock_session);
+                return self::validade($ip, $session)
+                    ? $session : self::login($ip, $usuario, $senha);
             }
             return self::login($ip, $usuario, $senha);
         } catch (\Exception $e) {
@@ -26,17 +31,20 @@ class LockSessionService
             'login' => $usuario,
             'password' => $senha
         ]);
-            session()->put('lock:session', $response->json('session'));
+        session()->put(self::getLockSession($ip), $response->json('session'));
 
-            return $response->json('session');
+        return $response->json('session');
     }
 
     // Verifica se a sessão atual ainda é válida
-    private static function validade($ip){
-        $session = session()->get('lock:session');
+    private static function validade($ip, $session){
         $route = 'http://' . $ip . '/session_is_valid.fcgi?session=' . $session;
         $response = Http::post($route, ['session' => $session]);
 
         return $response->json('session_is_valid');
+    }
+
+    private static function getLockSession($ip) {
+        return self::LOCK_SESSION . STR::replace('.', '_', $ip);
     }
 }
